1edb804c46062815e3cea49eee437d4076bcb8a9
santyyA <santy17apablaza@gmail.com>
2021-09-14 14:35:21 -0300

    a

---
+++ poo_ej1/bhaskara.js
@@ -0,0 +1,21 @@
+export default class Bhaskara {
+    coef_a;
+    coef_b;
+    ti_c;
+    constructor(){
+        
+    }
+    calcularRaiz1 (){
+        const bloque1 = (Math.pow(this.coef_b,2)) - (4*this.coef_a*this.ti_c)
+        const raiz = Math.sqrt(bloque1)
+        const resultado_r1 = (-this.coef_b + raiz) / (2*this.coef_a)
+        return resultado_r1;  
+    }
+    calcularRaiz2 (){
+        const bloque1 = (Math.pow(this.coef_b,2)) - (4*this.coef_a*this.ti_c)
+        const raiz = Math.sqrt(bloque1)
+        const resultado_r2 = (-this.coef_b - raiz) / (2*this.coef_a)
+        return resultado_r2;
+    }
+
+}
